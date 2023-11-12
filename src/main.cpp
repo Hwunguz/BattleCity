d2bc9634810c56bf366cd9b32a200023d79357f0
Hwunguz <sashaporyadin07@gmail.com>
2023-11-12 20:30:14 +0300

    first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,38 @@
 
 #include <iostream>
 
+GLfloat point[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f,
+};
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
+
+
+
 int g_windowSizeX = 640;
 int g_windowSizeY = 400;
 
@@ -17,6 +49,7 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
         glfwSetWindowShouldClose(pWindow, GL_TRUE);
     }
 }
+
 
 int main(void)
 {
@@ -57,13 +90,55 @@ int main(void)
 
     std::cout << "Opengl" << GLVersion.major << '.' << GLVersion.minor << std::endl;
 
-    glClearColor(1, 1, 0, 1);
+    glClearColor(0, 1, 0, 1);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram(); // создаём программу
+    glAttachShader(shader_program, vs); // добавляем в программу вершинный шейдер
+    glAttachShader(shader_program, fs); // добавляем в программу фрагментный шейдер
+    glLinkProgram(shader_program); // подключаем программу
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    GLuint point_vbo = 0;
+    glGenBuffers(1, &point_vbo); // сгенерировали буфер
+    glBindBuffer(GL_ARRAY_BUFFER, point_vbo); // подключили буфер
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW); // загрузили данные в буфер
+
+    GLuint color_vbo = 0;
+    glGenBuffers(1, &color_vbo); // сгенерировали буфер
+    glBindBuffer(GL_ARRAY_BUFFER, color_vbo); // подключили буфер
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW); // загрузили данные в буфер
+    
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    glEnableVertexAttribArray(0); // выбираем вектор  vbo
+    glBindBuffer(GL_ARRAY_BUFFER, point_vbo); // включаем вершинный буфер 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr); // 
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
     // Цикл отрисовки 
     while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
